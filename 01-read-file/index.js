f9904e29b319adf628765fe43f7482fce7b4f4a2
Oromashko <66132151+Oromashko@users.noreply.github.com>
2022-05-18 15:49:20 +0300

    feat: Develop tasks #1 and #2

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const { stderr, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+   fs.readFile(
+         path.join(__dirname, 'text.txt'),
+            'utf-8',
+            (err, data) => {
+                if (err) throw err;
+                stdout.write(data);
+            }
+        );
---
+++ 02-write-file/index.js
@@ -0,0 +1,37 @@
+const { stdin, stdout, stderr } = process;
+const fs = require('fs');
+const path = require('path');
+
+process.on('SIGINT', function() {
+   process.exit();});
+
+process.on('exit', code => {
+   if (code === 0) {
+       stdout.write('Удачи\n');
+   } else {
+       stderr.write(`Что-то пошло не так. Программа завершилась с кодом ${code}`);
+   }
+});
+function fileHandler(){
+
+   fs.open('input.txt', 'w', (err) => {
+       if(err) throw err;
+       console.log('Файл был создан');
+   });
+   
+}
+fileHandler();
+stdout.write('Как тебя зовут?\n');
+stdin.on('data', data => {
+               let str = data.toString();
+               str = str.replace('\n', '');
+               fs.appendFile(path.join(__dirname, 'input.txt'),str,
+               (err) => {
+               if (err) throw err;
+                  console.log('Файл был изменен');
+               });
+             /*  process.on('exit', () => stdout.write('Удачи!'));*/
+              
+
+});
+
