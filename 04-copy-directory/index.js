17ac84799c48ea02c1f7889817691faecc125267
Oromashko <66132151+Oromashko@users.noreply.github.com>
2022-05-22 20:57:47 +0300

    feat: Develop files creation without errors

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 const path = require('path');
-let origPath = __dirname;
+const fsPromises = fs.promises;
+
+let origPath =__dirname+'/files';
 let copyPath=__dirname+'/'+'files-copy';
 console.log("copyPath=",copyPath);
+console.log("origPath=",origPath);
+
 function pathHandler(pathName){
+   console.log("funstion pathHandler");
    if (typeof pathName === 'string'){
       fs.mkdir(pathName, function(){
       console.log(`Директория ${pathName} создана`);   
@@ -20,34 +25,54 @@ function fileHandler(fileName){
    }
    
 }
-fs.stat(copyPath, function(err) {
+/*fs.stat(copyPath, function(err) {
    if (!err) {
        console.log(`Директория ${copyPath} существует`);
    }
    else if (err.code === 'ENOENT') {
-      pathHandler(copyPath);
+      if (pathHandler(copyPath)){
        console.log(`Создание директории ${copyPath}`);
-   }
+      
+      }
+   };  
+});*/
+fsPromises.mkdir(copyPath, { recursive: true }).then(function() {
+   console.log(`Создание директории ${copyPath}`);
+}).catch(function() {
+   console.log(`Не могу создать директорию ${copyPath}`);
 });
 let i = 0;
 let fileMap = new Map();
+let pathCreated=false;
+/*while (!pathCreated) {
+fs.stat(copyPath, function(err) {
+   if (!err) {
+       pathCreated=true;
+       console.log("pathCreated true");
+   }
+     
+});
+};*/
 fs.readdir(copyPath, { withFileTypes: true }, (err, files) => {
-   files.forEach(file => {
-    if(file.isFile()){ 
-       fileMap.set(file.name, '1');
-       console.log("Filename"+file.name);
-      i++;
-       }
-   });
- });
+   if (err){console.log("Директория не существует",err);}
+   else{
+         files.forEach(file => {
+            if(file.isFile()){ 
+               fileMap.set(file.name, '1');
+               console.log("Filename "+file.name);
+               i++;
+            };     
+         });
+      }
+      });
 
  fs.readdir(origPath, { withFileTypes: true }, (err, files) => {
    files.forEach(file => {
       if(file.isFile()){ 
          fileName=copyPath+'/'+file.name;
          if (!fileMap.has(file.name)){
-            console.log("Create file"+file.name);
-            fileHandler(file.name);
+            console.log("Create file"+fileName);
+            fileHandler(fileName);
          };
       }
    })
