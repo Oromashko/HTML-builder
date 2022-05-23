ca45ae406fa5204c6a64e8c5de84407be627732a
Oromashko <66132151+Oromashko@users.noreply.github.com>
2022-05-23 23:43:06 +0300

    feat: Develop check if file exist and check timestamp (need some additional development)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,17 @@ const fsPromises = fs.promises;
 
 let origPath =__dirname+'/files';
 let copyPath=__dirname+'/'+'files-copy';
+let i = 0;
+let fileMap = new Map();
+let pathCreated=false;
+let toFileName;
+let fromFileName;
+let timeFromFile;
+let timeToFile;
+
+/*Проще всего сделать просто проверку существования файлов в директории copyPath чем связываться с ассоциативными массивами. Скорее всего ошибка изза точек в названиях файлов, которые нужно лиюо экранировать либо вообще нельзя использовать. Надо переделать.*/
+
+
 console.log("copyPath=",copyPath);
 console.log("origPath=",origPath);
 
@@ -25,25 +36,41 @@ function fileHandler(fileName){
    }
    
 }
-/*fs.stat(copyPath, function(err) {
+
+/*Проверяем существует  ли директория в которую должны скопироват. Создаем индекс (хеш) файлов из нее. 
+Если она не существует, создаём её.*/
+fs.stat(copyPath, function(err) {
    if (!err) {
-       console.log(`Директория ${copyPath} существует`);
+       console.log(`Директория ${copyPath} существует. Создаем хеш существующих файлов`);
+       fs.readdir(copyPath, { withFileTypes: true }, (err, files) => {
+            if (err){console.log("Директория не существует",err);}
+            else{
+                console.log("Директория существует");
+               files.forEach(file => {
+                     if(file.isFile()){ 
+                           fileMap.set(file.name, file.mtime);
+                           console.log("Filename "+file.name);
+                           i++;
+                     };     
+               });
+            }
+         });
+      
    }
    else if (err.code === 'ENOENT') {
       if (pathHandler(copyPath)){
        console.log(`Создание директории ${copyPath}`);
+       fsPromises.mkdir(copyPath, { recursive: true }).then(function() {
+         console.log(`Создание директории ${copyPath}`);
+      }).catch(function() {
+         console.log(`Не могу создать директорию ${copyPath}`);
+      });
       
       }
    };  
-});*/
-fsPromises.mkdir(copyPath, { recursive: true }).then(function() {
-   console.log(`Создание директории ${copyPath}`);
-}).catch(function() {
-   console.log(`Не могу создать директорию ${copyPath}`);
 });
-let i = 0;
-let fileMap = new Map();
-let pathCreated=false;
+
+
 /*while (!pathCreated) {
 fs.stat(copyPath, function(err) {
    if (!err) {
@@ -53,9 +80,10 @@ fs.stat(copyPath, function(err) {
      
 });
 };*/
-fs.readdir(copyPath, { withFileTypes: true }, (err, files) => {
+/*fs.readdir(copyPath, { withFileTypes: true }, (err, files) => {
    if (err){console.log("Директория не существует",err);}
    else{
+      console.log("Директория существует");
          files.forEach(file => {
             if(file.isFile()){ 
                fileMap.set(file.name, '1');
@@ -64,18 +92,47 @@ fs.readdir(copyPath, { withFileTypes: true }, (err, files) => {
             };     
          });
       }
-      });
+   });*/
 
  fs.readdir(origPath, { withFileTypes: true }, (err, files) => {
    files.forEach(file => {
       if(file.isFile()){ 
-         fileName=copyPath+'/'+file.name;
+         toFileName=path.join(copyPath, file.name);
+         fromFileName=path.join(origPath, file.name);
+         console.log("toFileName="+toFileName);
+         console.log("fromFileName"+fromFileName);
          if (!fileMap.has(file.name)){
-            console.log("Create file"+fileName);
-            fileHandler(fileName);
+            /*Такого файла не существует*/
+            console.log("Файла не существует. Создаем файл"+toFileName);
+            fileHandler(toFileName);
+            
+            fs.copyFile(fromFileName, toFileName, err => {
+            if(err) throw err; // не удалось скопировать файл
+               console.log('Файл успешно скопирован');
+            });
          };
-      }
-   })
-   });
+         
+         } else{
+            /* Файл существует. Проверяем дату изменения файла и обновляем файл, если он менялся*/
+                  console.log("Файл существует"+toFileName);
+                  
+                  timeFromFile=fileMap.get(file.name);
+                  fs.stat(toFileName, function(err, stats2) {
+                     if (err) {
+                        console.error(err);
+                        return;
+                     }
+                     timeToFile=stats2.mtime;
+                     console.log(`File1 date ${timeFromFile}`);
+                     console.log(`File2 date ${timeToFile}`);
+                  
+                  });
+               
+            }
+                        })
+                           });
+   
+
+   
  
 
