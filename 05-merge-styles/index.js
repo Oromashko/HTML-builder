88aee891cc50fc464aa0898c9a6bcc3a5a6b783d
Oromashko <66132151+Oromashko@users.noreply.github.com>
2022-05-24 20:38:18 +0300

    feat: Develop with sync functions. Finish

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const fsPromises = fs.promises;
 
-let bundleFileName = "bundle.css";
+let bundleFileName = "project-dist/bundle.css";
 let cssPathName = "styles";
 let bundleFileNameFull = path.join(__dirname, bundleFileName);
 let cssPathNameFull = path.join(__dirname, cssPathName);
 let cssFileExt = "css";
+let dataArray = new Array();
+let i=0;
 
 /*Функция создания файла*/
 function fileHandler(fileName){
@@ -21,28 +23,27 @@ function fileHandler(fileName){
 }
 /*Функция проверки расширения файла (только имя файла без папки) */
 function checkFileExt(fileName, ext){
-            console.log("function checkFileExt");
-            console.log("fileName="+fileName+"ext="+ext+"path.ext"+path.extname(fileName));
+            console.log("function checkFileExt ");
             let ext1=path.extname(fileName).replace('\.','');
+            console.log("fileName="+fileName+" ext="+ext+" path.ext="+ext1);
+            
           if (ext1=== ext) {
-             return true;
-             
-            console.log ("File ext is css");
-            }
+            console.log ("File ext is css"); 
+            return true;
+          }
           return false;  
 }
       
 
 /*функция добавления содержимого файла в конец другого файла */
-function appendFileToFile (fromFileName, toFileName){
-   let str ="";
-   fs.appendFile(toFileName,str,
-               (err) => {
-               if (err) throw err;
-                  console.log("Данные добавлены в файл"+toFileName);
-               });
+function appendDataToFile(dataArr, toFileName){
+   dataArr.forEach(element => {
+      /*console.log("Element="+element);*/
    
-   return false;
+   fs.appendFileSync(toFileName,element)
+   })
+
+   return true;
 
 }
 /*Функция удаления файла*/
@@ -68,23 +69,24 @@ function checkFileExist(fileName){
 
 if (checkFileExist(bundleFileNameFull)) {deleteFile(bundleFileNameFull)};
 if (fileHandler(bundleFileNameFull)===true){
-   console.log("Читаем директорию"+bundleFileNameFull);
+   console.log("Читаем директорию "+bundleFileNameFull);
    /*Читаем директорию стилей, проверяем расширение файла и если оно css  то записываем в конец файла bundle данные файла*/
 
-   fs.readdir(cssPathNameFull, { withFileTypes: true }, (err, files) => {
-      files.forEach(file => {
-         console.log("Filename=", file.name);
-      if(file.isFile()){  
-         if (checkFileExt(file.name, cssFileExt)){
-               console.log ("Добавляем содержимое файла"+file.name);
+   files = fs.readdirSync(cssPathNameFull, { withFileTypes: true });
+      files.forEach(file => {  
+         if(file.isFile()){  
+            if (checkFileExt(file.name, cssFileExt)){
+               console.log ("Добавляем содержимое файла "+file.name);
                fromCssFile = path.join(cssPathNameFull, file.name);
-               appendFileToFile(fromCssFile, bundleFileNameFull);
-
+               data = fs.readFileSync(fromCssFile, {encoding: 'utf8', flag:'r'});                   
+               dataArray.push(data);   
+            }
          }
-
-
-      }
       });
-   });
+   
 
-}
+   if (appendDataToFile(dataArray, bundleFileNameFull)){
+      console.log("Bundle is ready!");
+   };
+
+};
